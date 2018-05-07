119e3a1bc442011551fcf6c4634fb41cfcb64be5
Diego Fabricio <difagume@gmail.com>
2018-05-07 14:23:52 -0500

    se agrega nodemon y otras cosas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -8,7 +12,20 @@ const pool = new Pool({
   ssl: true
 });
 
-var app = express();
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+/* app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+})); */
 
 app.get('/times', (req, res) => {
   let result = ''
