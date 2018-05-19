64f274a760f243ba61845a1845285c927608e907
Diego Fabricio <difagume@gmail.com>
2018-05-19 16:36:30 -0500

    funcion para crear usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,31 +7,51 @@ var app = express();
  * Rutas
  */
 app.get('/', obtenerTodosLosUsuarios);
+app.post('/', crearUsuario);
 
 /**
  * Funciones
  */
 function obtenerTodosLosUsuarios(req, res, next) {
-  var t;
+  /* var t;
   db.one("SELECT count(*) FROM usuarios")
     .then(data => {
       //return parseInt(data.count);
       t = parseInt(data.count);
-    })
+    }) */
 
   db.any('select * from usuarios')
     .then(data => {
       res.status(200)
         .json({
           ok: true,
-          usuarios: data,
-          total: t
+          usuarios: data/*,
+          total: t*/
         });
     })
     .catch(err => {
       return next(err);
     });
   // .finally(db.$pool.end);
+}
+
+function crearUsuario(req, res, next) {
+  req.body.age = parseInt(req.body.age);
+  db.one('insert into usuarios(nombre, email, password, role, img, social)' +
+    'values(${nombre}, ${email}, ${password}, ${role}, ${img}, ${social})' +
+    'RETURNING *',
+    req.body)
+    .then(usuarioCreado => {
+      res.status(200)
+        .json({
+          ok: true,
+          usuario: usuarioCreado,
+          message: 'Usuario agregado'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 }
 
 module.exports = app;
